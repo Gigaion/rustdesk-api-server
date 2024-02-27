c3bef80a87ade68a7b127359ca67d12fb175b3d3
Gigaion <36256725+Gigaion@users.noreply.github.com>
2024-02-26 22:13:11 -0800

    Initial Commits - Translate settings.py fix to correct language code
    Initial Commits - Translate settings.py fix to correct language code

--- rustdesk_server_api/settings.py
+++ rustdesk_server_api/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-US'
+LANGUAGE_CODE = 'en'
 TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
